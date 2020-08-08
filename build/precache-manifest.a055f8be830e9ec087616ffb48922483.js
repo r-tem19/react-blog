174d471fbf997265b9ff90c3601af22d14e19fa6
r-tem19 <r-tem@outlook.com>
2020-08-08 17:45:01 +0300

    Initial commit

---
+++ build/precache-manifest.a055f8be830e9ec087616ffb48922483.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1875bb1216a1a6815e7eb771eba4100",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b6f87ab3dbe6d9e71b7",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f65294588dedcadeaa07",
+    "url": "/static/css/main.7e52c244.chunk.css"
+  },
+  {
+    "revision": "5b6f87ab3dbe6d9e71b7",
+    "url": "/static/js/2.de176139.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.de176139.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f65294588dedcadeaa07",
+    "url": "/static/js/main.c3f3414a.chunk.js"
+  },
+  {
+    "revision": "5330cfe6e7ce7a508d76",
+    "url": "/static/js/runtime-main.e6c4572c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0ad1d17f18f25d10000c84bc82e7bd9f",
+    "url": "/static/media/logo192.0ad1d17f.png"
+  },
+  {
+    "revision": "bedc016d5ea18b993654c2bb31a78f4f",
+    "url": "/static/media/pexels-eberhard-grossgasteiger-1004665.bedc016d.jpg"
+  },
+  {
+    "revision": "1e54b801926530bc5d004bdbfb39513c",
+    "url": "/static/media/pexels-mengliu-di-3064079.1e54b801.jpg"
+  }
+]);
